55b79c791a8716b7bbffae03598eea85ffceca8c
Ajiyba Nesij <ajiybanesij@hotmail.com>
2019-05-08 11:57:47 +0300

    Arduino Code Edited

--- Arduino/Esp32-GPS-Tracker.ino
+++ Arduino/Esp32-GPS-Tracker.ino
@@ -1,142 +1,152 @@
-
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
- 
-const char* ssid = "ssid";
-const char* password = "pass";
-String dateTimeData="";
 
 TinyGPSPlus gps;
 SoftwareSerial gpsSerial(16, 17); //rx,tx
+HTTPClient http;
 
+const char* ssid = "";          //Wifi Adı
+const char* password = "";  //Wifi Sifresi
 
+int LEDPIN = 23;
+int REDLED = 34;
+int YELLOWLED = 32;
 
 void setup()
 {
+  pinMode(LEDPIN, OUTPUT);
+  pinMode(REDLED,OUTPUT);
+  pinMode(YELLOWLED,OUTPUT);
   Serial.begin(115200);
-  gpsSerial.begin(9600);
+
 
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.println("Connecting to WiFi..");
+    Serial.println("Wifi Bağlanılıyor...");
   }
-  Serial.println("Connected to the WiFi network");
+  Serial.println("Wifi Bağlandı.");
+  delay(1000);
 
+  gpsSerial.begin(9600);
 }
 
 void loop()
 {
+  digitalWrite(LEDPIN, HIGH);
   while (gpsSerial.available() > 0)
     if (gps.encode(gpsSerial.read()))
       displayInfo();
   if (millis() > 5000 && gps.charsProcessed() < 10)
   {
-    Serial.println(F("No GPS detected"));
+    Serial.println(F("GPS Modülü Bulunamadı"));
     while (true)
       ;
   }
+  digitalWrite(LEDPIN, LOW);
   delay(100);
+  
 }
 
 void displayInfo()
 {
+  digitalWrite(YELLOWLED, LOW);
+
+  String LatData = "";
+  String LongData = "";
   if (gps.location.isValid())
   {
-    dateTimeData="";
-    Serial.print("Latitude: ");
-    Serial.println(gps.location.lat(), 6);
-    Serial.print("Longitude: ");
-    Serial.println(gps.location.lng(), 6);
-    Serial.print("Altitude: ");
-    Serial.println(gps.altitude.meters());
- 
-    GPSDate();
-    GPSTime(String(gps.location.lat(), 6),String(gps.location.lng(), 6));
+    //Serial.print("Altitude: ");
+    //Serial.println(gps.altitude.meters());
+    LatData = String(gps.location.lat(), 6);
+    LongData = String(gps.location.lng(), 6);
+    GPSDate(LatData, LongData);
+    digitalWrite(YELLOWLED, HIGH);
   }
   else
   {
-    Serial.println("Location: Not Available");
+    digitalWrite(YELLOWLED, LOW);
+    Serial.println("Lokasyon Bulunamadı.");
+    LatData = "";
+    LongData = "";
   }
   Serial.println();
   Serial.println();
-  delay(100);
 }
 
-void GPSDate()
+void GPSDate(String LatData, String LongData)
 {
-  Serial.print("Date: ");
+  String DateData = "";
   if (gps.date.isValid())
   {
-    Serial.print(gps.date.month());
-    Serial.print("/");
-    Serial.print(gps.date.day());
-    Serial.print("/");
-    Serial.println(gps.date.year());
-    String dateDatas=String(gps.date.month())+"="+String(gps.date.day())+"="+String(gps.date.year());
-    dateTimeData=dateTimeData+""+dateDatas;
-  
+    DateData = String(gps.date.month()) + "=" + String(gps.date.day()) + "=" + String(gps.date.year());
+    GPSTime(LatData, LongData, DateData);
   }
   else
   {
-    Serial.println("Not Available");
+    DateData = "";
+    Serial.println("Tarih Bulunamadı.");
   }
 }
-void GPSTime(String lats,String lngs)
+void GPSTime(String LatData, String LongData, String DateData)
 {
-  Serial.print("Time: ");
+  String DateTime = "";
+  String TimeData = "";
   if (gps.time.isValid())
   {
-    if (gps.time.hour() < 10)
-      Serial.print(F("0"));
-    Serial.print(gps.time.hour());
-    Serial.print(":");
-    if (gps.time.minute() < 10)
-      Serial.print(F("0"));
-    Serial.print(gps.time.minute());
-    Serial.print(":");
-    if (gps.time.second() < 10)
-      Serial.print(F("0"));
-    Serial.print(gps.time.second());
-    Serial.print(".");
-    if (gps.time.centisecond() < 10)
-      Serial.print(F("0"));
-    Serial.println(gps.time.centisecond());
-
-    String timeDatas=String(gps.time.hour())+"="+String(gps.time.minute())+"="+String(gps.time.second() );
-    dateTimeData=dateTimeData+"-"+timeDatas;
-    Serial.println("********");
-    Serial.println(dateTimeData);
-    SendData(lats,lngs,dateTimeData);
+    String saat = "";
+    String dakika = "";
+    String saniye = "";
+    if (gps.time.hour() < 10) {
+      saat = "0" + String(gps.time.hour());
+    } else {
+      saat = String(gps.time.hour());
+    }
+    if (gps.time.minute() < 10) {
+      dakika = "0" + String(gps.time.minute());
+    } else {
+      dakika = String(gps.time.minute());
+    }
+    if (gps.time.second() < 10) {
+      saniye = "0" + String(gps.time.second());
+    } else {
+      saniye = String(gps.time.second());
+    }
+    TimeData = String(saat) + "=" + String(dakika) + "=" + String(saniye);
+    DateTime = String(DateData) + "-" + String(TimeData);
+    SendData(LatData, LongData, DateTime);
   }
   else
   {
-    Serial.println("Not Available");
+    TimeData = "";
+    Serial.println("Zaman Bulunamadı.");
   }
 }
 
-void SendData(String lats,String lngs,String datetimew){
-  if ((WiFi.status() == WL_CONNECTED)) { //Check the current connection status
- 
-    HTTPClient http;
-    String URL="";
+void SendData(String LatData, String LongData, String DateTime) {
+  Serial.println(LatData);
+  Serial.println(LongData);
+  Serial.println(DateTime);
+  Serial.println();
+  
 
-    http.begin(URL); //Specify the URL
-    int httpCode = http.GET();                                        //Make the request
- 
-    if (httpCode > 0) { //Check for the returning code
- 
-        String payload = http.getString();
-        Serial.println(httpCode);
-        Serial.println(payload);
-      }
- 
-    else {
-      Serial.println("Error on HTTP request");
+  if ((WiFi.status() == WL_CONNECTED)) {
+    String URL = "/api/add/" + LatData + "/" + LongData + "/" + DateTime;
+    http.begin(URL);
+    int httpCode = http.GET();
+    if (httpCode > 0) {
+      digitalWrite(REDLED,HIGH);
+      String payload = http.getString();
+      Serial.println(httpCode);
+      Serial.println(payload);
+      delay(1000);
+      digitalWrite(REDLED,LOW);
     }
- 
-    http.end(); //Free the resources
+    else {
+      Serial.println("HTTP isteği hatalı!");
+    }
+    http.end();
   }
 }
